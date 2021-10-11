d0949b3a7ea839347f67086cac833c088e61fcb6
FABIANA NOVAIS DOS SANTOS <fabiana.novais@sme.prefeitura.sp.gov.br>
2021-10-11 12:16:13 -0300

    criação da rota de deleção pelo administrador

--- routes/admin/show.js
+++ routes/admin/show.js
@@ -5,7 +5,11 @@ const authToken = require('./../../middlewares/authToken')
 const mongoose = require('mongoose')
 
 require('./../../models/usuarios')
+require('./../../models/usuarios_subSchemas/fisico')
+require('./../../models/usuarios_subSchemas/ong')
 const Usuarios = mongoose.model('usuarios')
+const Fisicos = mongoose.model('fisicos')
+const Ongs = mongoose.model('ongs')
 
 //TODO: testar rota
 router.post('/user/:tipo',authToken.obrigatorio, async (req, res) => {
@@ -65,6 +69,103 @@ router.post('/user/:tipo',authToken.obrigatorio, async (req, res) => {
 
 })
 
+//TODO: rota de deleção de usuarios
+router.post('/delete/:tipo', authToken.obrigatorio, async (req, res) => {
 
+  await Usuarios.findOne({_id: req.data.id})
+  .then(admin => {
+    if (admin.tipo === "adm"){
+      return
+    }else{
+
+      console.log(`rota de deleção foi acessada pelo usuário ${admin._id}, não administrador`)
+
+      res.status(401).send({
+        msg: "usuário não é um administrador"
+      })
+    }
+  })
+  .catch(e => {
+
+    console.log(`erro ao encontrar usuário:::${e}`)
+
+    res.status(500).send({
+      msg: "erro ao encontrar usuário"
+    })
+  })
+
+  var delUser
+
+  await Usuarios.findOne({_id: req.body.id})
+  .then(async user => {
+    if (user.tipo === "ong"){
+
+      // * deleção de dado de ong
+
+      await Ongs.deleteOne({_id: user.ong})
+      .then(delOng => {
+
+        console.log(`ong ${delOng._id} deletada`)
+
+      })
+      .catch(e => {
+        console.log(`erro ao deletar dados de ong::::${e}`)
+
+        res.status(500).send({
+          msg: "erro ao deletar dados de ong"
+        })
+      })
+
+    }else if (user.tipo === "nrm"){
+    
+      // * deleção de dado de pessoa fisica
+
+      await Fisicos.deleteOne({_id: user.fisico})
+      .then(delFisico => {
+
+        console.log(`dados de pessoa física ${delFisico._id} deletado`)
+        
+      })
+      .catch(e => {
+
+        console.log(`erro ao deletar dados de pessoa física:::${e}`)
+
+        res.status(500).send({
+          msg: "erro ao deletar dados de pessoa física"
+        })
+
+      })
+
+    }
+
+    // * deleção de dados gerais
+
+    Usuarios.deleteOne({_id: user._id})
+    .then(() => {
+      console.log(`usuário ${user._id} deletado`)
+
+      res.status(200).send({
+        msg: "usuário deletado com sucesso"
+      })
+    })
+    .catch(e => {
+
+      console.log(`erro ao deletar usuário:::${e}`)
+
+      res.status(500).send({
+        msg: "erro ao deletar usuário"
+      })
+    })
+  })
+  .catch(e => {
+
+    console.log(`erro ao encontrar usuário para deletar:::${e}`)
+
+    res.status(500).send({
+      msg: "erro ao encontrar usuário para deletar"
+    })
+  })
+
+})
 
 module.exports = router

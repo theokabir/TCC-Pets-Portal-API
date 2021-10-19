a1b51e30b6c20482a60edf5fa5e931ba87f4bd02
FABIANA NOVAIS DOS SANTOS <fabiana.novais@sme.prefeitura.sp.gov.br>
2021-10-19 15:19:52 -0300

    amostragem de animais

--- routes/animais/animais.js
+++ routes/animais/animais.js
@@ -4,7 +4,9 @@ const mongoose = require('mongoose')
 const authToken = require("./../../middlewares/authToken")
 
 require('./../../models/animais')
+require('./../../models/messages')
 const Mensagens = mongoose.model('mensagens')
+const Animais = mongoose.model('animais')
 
 const cadastroRouter = require('./cadastro')
 const edicaoRouter = require('./edicao')
@@ -16,8 +18,32 @@ router.use("/delete", deleteRouter)
 
 //TODO: testar rota
 // ? rota com login obrigatorio
-router.get('/', authToken.obrigatorio, (req, res) => {
+router.post('/', authToken.obrigatorio, (req, res) => {
     //TODO: rota de pesquisa
+})
+
+router.post('/:id', authToken.obrigatorio, async (req, res) => {
+
+    try{
+
+        var animal = await Animais.findOne({_id: req.params.id})
+        .populate({
+            path: "responsavel",
+            select: "_id imagem"
+        })
+        var me = animal.responsavel._id == req.data.id
+        res.status(200).send({
+            animal,
+            me
+        })
+
+    }catch(e){
+        console.log(`erro ao requisitar animal:::${e}`)
+        res.status(500).send({
+            msg: "erro ao encontrar animal"
+        })
+    }
+
 })
 
 

16fe5262dce4d3b93830bed24d4a80edeabaadac
theokabir <theogame4@gmail.com>
2021-11-03 18:19:52 -0300

    edição de eventos

--- routes/eventos/editar.js
+++ routes/eventos/editar.js
@@ -8,9 +8,57 @@ const upload = require('./../../middlewares/upload')
 require('./../../models/eventos')
 const Eventos = mongoose.model('eventos')
 
-router.post("/", authToken.obrigatorio, upload.single("banner"), gcs.upload, async (req, res) => {
+router.post("/", authToken.obrigatorio, async (req, res) => {
 
   var evento
+  console.log(req.body)
+  console.log(req.headers)
+
+  try{
+
+    evento = await Eventos.findOne({_id: req.body.evento})
+
+    if (evento.responsavel != req.data.id){
+      var err  = {
+        code: 401,
+        msg: "usuário não p-opde editar este evento"
+      }
+
+      throw err
+    }
+
+    evento.nome = req.body.nome
+    evento.local = req.body.local
+    evento.data = req.body.data
+    evento.observacao = req.body.observacao
+    evento.especies = req.body.especies
+    evento.editado = true
+
+    var newEvento = await evento.save()
+    console.log("editado evento:::\n" + newEvento)
+
+
+    console.log("evento editado")
+
+    res.status(200).send({
+      msg: "evento editado"
+    })
+
+
+  }catch(e){
+
+    console.log("erro ao editar evento:: " + e)
+
+    res.status(e.code || 500).send({
+      msg: "erro ao editar evento"
+    })
+
+  }
+
+})
+
+router.post("/foto", authToken.obrigatorio, upload.single("banner"), gcs.upload, async (req, res) => {
+
   var oldImage
 
   try{
@@ -26,42 +74,23 @@ router.post("/", authToken.obrigatorio, upload.single("banner"), gcs.upload, asy
       throw err
     }
 
-    oldImage = evento.banner || undefined
-
-    evento.nome = req.body.nome
-    evento.local = req.body.local
-    evento.data = req.body.data
-    evento.observacao = req.body.observacao
-    evento.especies = req.body.especies
+    oldImage = evento.banner.split("/").pop()
     evento.banner = req.newFile
-    evento.editado = true
+    await evento.save()
+    gcs.delete(oldImage)
 
-    var newEvento = await evento.save()
-    console.log("editado evento:::\n" + newEvento)
-    if (evento.banner)
-      gcs.delete(evento.banner.split("/").pop())
-
-
-    console.log("evento editado")
+    console.log("imagem editada com sucesso")
 
     res.status(200).send({
-      msg: "evento editado"
+      msg: "imagem editada com sucesso"
     })
-
 
   }catch(e){
-
-    console.log("erro ao editar evento:: " + e.msg || e)
-
-    if (evento.banner == req.newFile)
-      gcs.delete(evento.banner.split("/").pop())
-    else
-      gcs.delete(oldImage.split("/").pop())
+    console.log("erro ao editar imagem do evento:: " + e.msg || e)
 
     res.status(e.code || 500).send({
-      msg: "erro ao editar evento"
+      msg: "erro ao editar imagem do evento"
     })
-
   }
 
 })

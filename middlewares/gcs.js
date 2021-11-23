13352873bf110087dd27e3cf4c6ad9bc5cfae963
theokabir <theogame4@gmail.com>
2021-11-22 23:22:16 -0300

    correções

--- middlewares/gcs.js
+++ middlewares/gcs.js
@@ -16,37 +16,41 @@ const bucket = storage.bucket(process.env.GCS_BUCKET)
 
 exports.upload = async (req, res, next) => {
 
-  var file = req.file
-  console.log(file)
-  var publicUrl = ""
+  if (req.file){
+    var file = req.file
+    console.log(file)
+    var publicUrl = ""
+    
+    const extensao = file.originalname.split('.').pop()
+    await bcrypt.hash(file.originalname, 10, (errBcrypt, hash)=>{
+        if (errBcrypt){
+            res.status(500).send({msg: "nome da imagem não foi criptografado"})
+        }else{
   
-  const extensao = file.originalname.split('.').pop()
-  await bcrypt.hash(file.originalname, 10, (errBcrypt, hash)=>{
-      if (errBcrypt){
-          res.status(500).send({msg: "nome da imagem não foi criptografado"})
-      }else{
-
-          var blob = bucket.file(hash.replace(/\W/g,"") + "." +extensao)
-          const blobStream = blob.createWriteStream();
-
-          blobStream.on("error", (err) => {
-            res.status(500).send({ message: err.message });
-          });
-
-          blobStream.on("finish", async (data) => {
-            publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`
-
-            console.log(publicUrl)
-
-            req.newFile = publicUrl
-            next()
-
-          })
-          
-          blobStream.end(req.file.buffer);
-          console.log("finished")
-      }
-  })
+            var blob = bucket.file(hash.replace(/\W/g,"") + "." +extensao)
+            const blobStream = blob.createWriteStream();
+  
+            blobStream.on("error", (err) => {
+              res.status(500).send({ message: err.message });
+            });
+  
+            blobStream.on("finish", async (data) => {
+              publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`
+  
+              console.log(publicUrl)
+  
+              req.newFile = publicUrl
+              next()
+  
+            })
+            
+            blobStream.end(req.file.buffer);
+            console.log("finished")
+        }
+    })
+  }else{
+    res.status(500).send({msg: "foto  inexistente"})
+  }
 }
 
 exports.delete = async (foto) => {
